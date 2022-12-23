e73a86cb8f943a6a0ff009a4dc123ccee904adb7
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-12-23 19:52:26 -0300

    feat(sequelize): set up initial configurations

---
+++ app/backend/src/database/config/config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const options = {
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DB_NAME,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: process.env.DEBUG === 'true',
+};
+
+module.exports = {
+  development: options,
+  test: options,
+  production: options,
+};
