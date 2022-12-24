99ef6ada750ae8da20eb4668af907fae1d7e1c20
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-12-23 21:50:38 -0300

    feat: partial implementation of model User

---
+++ app/backend/src/database/models/user.js
@@ -0,0 +1,55 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      unique: true,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING(255),
+      defaultValue: null,
+    },
+    mobile: {
+      type: DataTypes.STRING(255),
+      defaultValue: null,
+    },
+    departament: {
+      type: DataTypes.STRING(255),
+      defaultValue: null,
+    },
+    verificationCode: {
+      type: DataTypes.STRING(255),
+      defaultValue: null,
+    },
+    emailChecked: {
+      type: DataTypes.TINYINT(1),
+      defaultValue: 0,
+    },
+    cashforceAdm: {
+      type: DataTypes.TINYINT(1),
+      defaultValue: 0,
+    },
+  }, {
+    underscored: false,
+    timestamps: true,
+    tableName: 'users',
+  });
+
+  // User.associate = (models) => {
+  //   User.belongsTo(models.Order, {
+  //     foreignKey: 'userId',
+  //   });
+  // };
+
+  return User;
+};
