bc009492ebafd36eb660db7b906093cb438a1a1e
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-12-24 16:25:37 -0300

    feat(express): add route to list NFs

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const { Order, Buyer, Provider, Cnpj } = require('./database/models');
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', async (_req, res) => {
-  res.send('OK');
-})
+  const allNfs = await Order.findAll({
+      attributes: ['nNf',
+      'emissionDate',
+      'value',
+      'orderStatusBuyer'
+    ],
+    include: [
+      { model: Buyer,
+        as: 'buyer',
+        attributes: ['name']
+      },
+      { model: Provider,
+        as: 'provider',
+        attributes: ['id']
+      },
+    ]
+  });
+
+  return res.status(200).json(allNfs);
+});
 
 module.exports = app;
