c8a41ea622007e667a970b88c2eadce95bd711f4
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-12-27 21:57:54 -0300

    chore(App): styling the component

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,23 +1,66 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+  import { RouterLink, RouterView } from 'vue-router';
+  import './assets/base.css';
 </script>
 
 <template>
-  <div>
-    <header>
-      <div class="wrapper">
-        <img src='' alt='cashforce logo' />
+  <div class="d-flex">
 
-        <nav>
-          <RouterLink to="/">Notas Fiscais</RouterLink>
+    <section class="menu-section">
+      <div>
+        <img class="p-4" src='./assets/logo.png' alt='Cashforce logo' />
+        <nav class="pt-1 d-flex flex-column">
+          <RouterLink to="/">
+            <div class="d-flex gap-4 menu-item align-items-center">
+              <div class="vl"></div>
+              <img src='./assets/icons/handshake-green.svg' alt='aperto de mãos' />
+              <span>Notas fiscais</span>
+            </div>
+          </RouterLink>
         </nav>
       </div>
-    </header>
+    </section>
 
-    <RouterView />
+    <section class="flex-grow-1 content-section">
+      <header></header>
+      <hr />
+      <RouterView />
+    </section>
+
   </div>
 </template>
 
 <style scoped>
+  .menu-section {
+    width: 288px;
+    background: #FBFDFE;
+    font-size: 12px;
+  }
 
+  a, a:hover {
+    text-decoration: none;
+    color: var(--text-theme);
+    font-weight: 500;
+  }
+
+  .vl {
+    border-left: 4px solid var(--text-theme);
+    border-radius: 4px;
+    height: 64px;
+  }
+
+  header {
+    height: 64px;
+  }
+
+  hr {
+    color: #DFE2EB;
+  }
+
+  .content-section {
+    background: #fff;
+    box-shadow: 0px 10px 30px rgba(225, 229, 236, 0.5);
+    border-radius: 15px 0px 0px 15px;
+    min-height: 100vh;
+  }
 </style>
