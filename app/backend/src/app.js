c97c790e4aaf09f28e81a7a338f17ae1d43ce4c2
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-12-29 18:53:34 -0300

    refactor: apply MSC architecture

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -1,47 +1,10 @@
 const express = require('express');
-const { Order, Buyer, Provider, Cnpj } = require('./database/models');
+const cors = require('cors');
+const router = require('./router');
 
 const app = express();
-const cors = require('cors');
-app.use(cors());
-
 app.use(express.json());
-
-
-app.get('/provider/:id', async (req, res) => {
-  const { id } = req.params;
-
-  const providerDetails = await Provider.findOne({
-    where: { id },
-    attributes: ['name', 'tradingName'],
-    include: [
-      { model: Cnpj, as: 'cnpj', attributes: ['cnpj'] },
-    ]
-  });
-
-  return res.status(200).json(providerDetails);
-});
-
-app.get('/', async (_req, res) => {
-  const allNfs = await Order.findAll({
-      attributes: ['nNf',
-      'emissionDate',
-      'value',
-      'orderStatusBuyer'
-    ],
-    include: [
-      { model: Buyer,
-        as: 'buyer',
-        attributes: ['name']
-      },
-      { model: Provider,
-        as: 'provider',
-        attributes: ['id', 'name']
-      },
-    ]
-  });
-
-  return res.status(200).json(allNfs);
-});
+app.use(cors());
+app.use(router);
 
 module.exports = app;
