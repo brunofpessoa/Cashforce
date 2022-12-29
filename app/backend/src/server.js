e8dcd057b0c0285969dcad4695dfa4cf900d40cb
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-12-29 18:52:40 -0300

    fix: add default port

--- app/backend/src/server.js
+++ app/backend/src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-const port = process.env.API_PORT;
+const port = process.env.API_PORT || 3000;
 
 app.listen(port, () => console.log('Listening on port', port));
